d7e1061325ab3629f50e37670b9585b5bb175693
sanip <saneprijal@gmail.com>
2020-03-12 15:03:04 +0545

    import settings

--- product/views.py
+++ product/views.py
@@ -13,6 +13,7 @@ from django.template import RequestContext
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from django.conf import settings
 
 
 def handler404(request, *args, **argv):
